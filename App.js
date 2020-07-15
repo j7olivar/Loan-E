9f5d05c14579c81e8d83701642fdcc6338a3184f
j7olivar <59585842+j7olivar@users.noreply.github.com>
2020-07-14 20:04:47 -0700

    Installed firebase and the api keys on App.js
    Next thing to do is figure out how to setup the database

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { StyleSheet, Text, View, TextInput, Button, Modal, ScrollView, FlatList 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 import Header from './components/Header'
+import ApiKeys from './Constants/ApiKeys'
+import * as firebase from 'firebase'
 
-export default function App() {
-
-  
+export default function App(){
   const [courseGoals, setCourseGoals] = useState([])
-  const[isAddMode, setIsAddMode] = useState(false);
- 
+  const [isAddMode, setIsAddMode] = useState(false);
+   //makes sure firebase wasnt intialized before so it doesnt call it again
+   //and give an error 
+  if(!firebase.apps.length){
+      //this is where we setup firebase and call it the first time  
+      firebase.initializeApp(ApiKeys.FirebaseConfig)
+  }
+  
   const addGoalHandler = (goalTitle, interestRate, years, paidOff) => {
     //setCourseGoals([...courseGoals, enteredGoal])
     setCourseGoals(prevGoals => [
@@ -28,45 +34,41 @@ export default function App() {
 
   const cancelGoalAdditionHandler = () =>{
     setIsAddMode(false);
-
   }
 
   const removeGoalHandler = goalId => {
         setCourseGoals(currentGoals=>{
             return currentGoals.filter(goal => goal.id !== goalId)
-        })}
-
+        })
+  }
+  
   return (
 
-    <View style={styles.screen} >
-
-        <Header title="Student Loan Calculator"/>
-    
-      <View style= {{padding:20}}> 
-      <Text style = {styles.title}> LOANS: </Text>
+    <View style={styles.screen}>
+  
+      <Header title="Student Loan Calculator"/>
       
-      <GoalInput visible={isAddMode}
+      <View style= {{padding:20}}> 
+        <Text style = {styles.title}> LOANS: </Text>
+        
+        <GoalInput visible={isAddMode}
         addGoalHandler={addGoalHandler} 
-        onCancel={cancelGoalAdditionHandler}
-      />
-
-      <FlatList 
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={itemData => (
-        <GoalItem
-          onDelete={removeGoalHandler.bind(this, itemData.item.id)} 
-          title={itemData.item.value}
-          subInterest={itemData.item.interest}
-          subPaid={itemData.item.paidOff}
-          subYears={itemData.item.years}
-          />)}
-      />
-      <Button title="Add New Loan" onPress={() => setIsAddMode(true)}/>
+        onCancel={cancelGoalAdditionHandler}/>
+  
+        <FlatList 
+          keyExtractor={(item, index) => item.id}
+          data={courseGoals}
+          renderItem={itemData => (
+          <GoalItem
+            onDelete={removeGoalHandler.bind(this, itemData.item.id)} 
+            title={itemData.item.value}
+            subInterest={itemData.item.interest}
+            subPaid={itemData.item.paidOff}
+            subYears={itemData.item.years}
+          />)}/>
+        <Button title="Add New Loan" onPress={() => setIsAddMode(true)}/>
       </View>
-    </View >
-   
-  )
+    </View >)
 }
 
 const styles = StyleSheet.create({
---
+++ Constants/ApiKeys.js
@@ -0,0 +1,12 @@
+export default {
+    FirebaseConfig: {
+        apiKey: "AIzaSyCYjqgKppX4GuD7xuGj_saUAiqP_Z1vGnw",
+        authDomain: "loane-9156e.firebaseapp.com",
+        databaseURL: "https://loane-9156e.firebaseio.com",
+        projectId: "loane-9156e",
+        storageBucket: "loane-9156e.appspot.com",
+        messagingSenderId: "625989026940",
+        appId: "1:625989026940:web:b3f335d2eba404632c35f9",
+        measurementId: "G-LZSKGCLNY8"
+    }
+}
