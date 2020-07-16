9facdaa58d49556fb9473badb9fb3cf5640c2bd0
j7olivar <59585842+j7olivar@users.noreply.github.com>
2020-07-16 12:14:25 -0700

    this is the one I reverted to
    before I messed up my code

--- App.js
+++ App.js
@@ -4,58 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button, Modal, ScrollView, FlatList 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 import Header from './components/Header'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import LogIn from './pages/LogIn'
-import Loans from './pages/Loans'
-import ApiKeys from './Constants/ApiKeys'
+//import ApiKeys from './Constants/ApiKeys'
 import * as firebase from 'firebase'
 
-export default function App(){
-  const [name,setName]= useState('')
-  
-   //makes sure firebase wasnt intialized before so it doesnt call it again
-   //and give an error 
-  if(!firebase.apps.length){
-      //this is where we setup firebase and call it the first time  
-      firebase.initializeApp(ApiKeys.FirebaseConfig)
-  }
-
-  function componentDidMount(){
-    this.authListener()
-  }
-
-  //for loggining to the app/ when authenication state changes
-  function authListener(){
-    firebase.auth().onAuthStateChanged((user) => {
-      //when logging in
-      if(user){
-        setName(user)
-      }
-      //when logging out
-      else{
-        setName(null)
-      }
-    })
-  }
-  //this is going to hold our screens
-  const Stack = createStackNavigator()
-
-  //this is where we decide where to send them,
-  //either to the login screen or the home screen
-
-  return(
-    <View>
-      { {name} ? (<Loans/>) : (<LogIn/>)}
-    </View>
-    
-  ) 
-
-=======
-
-=======
-
->>>>>>> parent of 9f5d05c1... Installed firebase and the api keys on App.js
 export default function App() {
 
   
@@ -118,12 +69,7 @@ export default function App() {
     </View >
    
   )
-<<<<<<< HEAD
->>>>>>> parent of 9f5d05c1... Installed firebase and the api keys on App.js
-=======
->>>>>>> parent of 9f5d05c1... Installed firebase and the api keys on App.js
 }
-
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
