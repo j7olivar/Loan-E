4de340e358136e198f4b97d283ed2026ee074514
j7olivar <59585842+j7olivar@users.noreply.github.com>
2020-07-16 11:47:07 -0700

    not a good state
    this stack has a bug I cant figure out so i have to merge it before I go back to another one

---
+++ pages/LogIn.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import * as firebase from 'firebase'
+import 'react-native-gesture-handler'
+import {NavigationContainer, createStackNavigator} from 'react-navigation'
+
+export default function LogIn(){
+    
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  }
+  
+});
