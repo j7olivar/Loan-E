6c07a615e1a514df744d1aab62493a6429441b0a
Sam Kapusta <41392930+cabbagelover25@users.noreply.github.com>
2020-08-04 22:06:46 -0700

    Update FavoriteMealScreen.js

--- Screens/Main Screens/FavoriteMealScreen.js
+++ Screens/Main Screens/FavoriteMealScreen.js
@@ -14,11 +14,56 @@ import {
 
 
   const FavoriteMealScreen = (props) => {
+    return (
+      <View style={styles.screen}>
+        <Header title="Student Loan Calculator" />
+        <View style={styles.screen}>
+        <LineChart
+  data={{
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+    ],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43],
+        strokeWidth: 2,
+      },
+    ],
+  }}
+  width={Dimensions.get('window').width - 16}
+  height={220}
+  chartConfig={{
+    backgroundColor: '#1cc910',
+    backgroundGradientFrom: '#eff3ff',
+    backgroundGradientTo: '#efefef',
+    decimalPlaces: 2,
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    style: {
+      borderRadius: 16,
+    },
+  }}
+  style={{
+    marginVertical: 8,
+    borderRadius: 16,
+  }}
+/>
+        </View>
+      </View>
+    );
+  };
+  const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  });
 
-const FavoriteMealScreen = (props) => {
-	return (
-		<View style={styles.screen}>
-			<Header title="Student Loan Calculator" />
-			<View style={styles.screen}>
+
 
 export default FavoriteMealScreen;
